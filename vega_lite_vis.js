c494c89f24aa025e0970fa99755120a03c8f9aab
Deeksha Sridhar <dsri0006@student.monash.edu>
2023-09-20 21:53:50 +0800

    update js file

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://raw.githubusercontent.com/Deeksha1703/FIT3179_W9/main/chloropleth_map.vg.json";
+var vg_1 = "chloropleth_map.vg.json";
 vegaEmbed("#chloropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
